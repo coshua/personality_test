84bc05bcffcfc03d23d345d029eb45154acf13d4
Dongju <bitlez3470@gmail.com>
2020-09-11 18:48:59 +0900

    audio test

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -2,6 +2,6 @@ import memories from "../audios/memories.mp3";
 import tomorrow from "../audios/tomorrow.mp3";
 import ukulele from "../audios/ukulele.mp3";
 
-const Playlist = [memories, tomorrow, ukulele, memories];
+const Playlist = [memories, tomorrow, ukulele, memories, tomorrow];
 
 export default Playlist;
